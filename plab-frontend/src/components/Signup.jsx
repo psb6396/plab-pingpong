3b9764869e86fb1e723555a5b0fdbe50cd754a65
psb6396 <psb6396@naver.com>
2024-12-24 19:00:44 +0900

    f

--- plab-frontend/src/components/Signup.jsx
+++ plab-frontend/src/components/Signup.jsx
@@ -1,10 +1,14 @@
+import { TextField, Button, Container, Typography, CircularProgress } from '@mui/material'
+import { useCallback } from 'react'
+import { useState } from 'react'
 const Signup = () => {
    const [email, setEmail] = useState('')
    const [nick, setNick] = useState('')
    const [password, setPassword] = useState('')
    const [confirmPassword, setConfirmPassword] = useState('')
    const [isSignupComplete, setIsSignupComplete] = useState(false) // 회원가입 완료 상태 추가
-
+   const error = false
+   const loading = false
    const handleSignup = useCallback(() => {
       if (!email.trim() || !nick.trim() || !password.trim() || !confirmPassword.trim()) {
          alert('모든 필드를 입력해주세요!')
@@ -14,17 +18,32 @@ const Signup = () => {
          alert('비밀번호가 일치하지 않습니다!')
          return
       }
-      dispatch(registerUserThunk({ email, nick, password }))
-         .unwrap()
-         .then(() => {
-            //회원가입 성공시
-            setIsSignupComplete(true) //회원가입 완료 상태 true로 변경
-         })
-         .catch((error) => {
-            //회원가입중 에러 발생시
-            console.error('회원가입 에러:', error)
-         })
-   }, [email, nick, password, confirmPassword, dispatch])
+   })
+   return (
+      <Container maxWidth="sm">
+         <Typography variant="h4" gutterBottom>
+            회원가입
+         </Typography>
+
+         {error && (
+            <Typography color="error" align="center">
+               {error}
+            </Typography>
+         )}
+
+         <TextField label="이메일" variant="outlined" fullWidth margin="normal" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+         <TextField label="사용자 이름" variant="outlined" fullWidth margin="normal" value={nick} onChange={(e) => setNick(e.target.value)} />
+
+         <TextField label="비밀번호" variant="outlined" type="password" fullWidth margin="normal" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+         <TextField label="비밀번호 확인" variant="outlined" type="password" fullWidth margin="normal" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+
+         <Button variant="contained" color="primary" onClick={handleSignup} fullWidth disabled={loading} style={{ marginTop: '20px' }}>
+            {loading ? <CircularProgress size={24} /> : '회원가입'}
+         </Button>
+      </Container>
+   )
 }
 
 export default Signup
