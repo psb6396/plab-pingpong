339dfa05e82a347d8bf045a924f9273a21b349cc
psb6396 <psb6396@naver.com>
2024-12-29 14:30:20 +0900

    asdf

--- plab-frontend/src/api/plabApi.js
+++ plab-frontend/src/api/plabApi.js
@@ -58,3 +58,12 @@ export const logoutUser = async () => {
 }
 
 //내 프로필 가져오기
+export const getProfile = async () => {
+  try {
+    const response = await plabApi.get(`/page/profile`)
+    return response
+  } catch (error) {
+    console.error(`API Request 오류: ${error.message}`)
+    throw error
+  }
+}
--- plab-frontend/src/features/pageSlice.js
+++ plab-frontend/src/features/pageSlice.js
@@ -1,1 +1,42 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { getProfile } from '../api/plabApi'
+
+export const getProfileThunk = createAsyncThunk(
+  'page/getProfile',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await getProfile()
+      return response.data.user
+    } catch (error) {
+      return rejectWithValue(
+        error.response?.data?.message || '프로필 정보 불러오기 실패'
+      )
+    }
+  }
+)
+
+const pageSlice = createSlice({
+  name: 'page',
+  initialState: {
+    user: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    //내 프로필 가져오기
+    builder
+      .addCase(getProfileThunk.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(getProfileThunk.fulfilled, (state, action) => {
+        state.loading = false
+        state.user = action.payload
+      })
+      .addCase(getProfileThunk.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload
+      })
+  },
+})
