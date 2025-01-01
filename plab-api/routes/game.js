10ffdf050025e26de904e158f15454c8a5198952
psb6396 <psb6396@naver.com>
2025-01-02 02:41:05 +0900

    asd

--- plab-api/routes/game.js
+++ plab-api/routes/game.js
@@ -5,4 +5,18 @@ const { isLoggedIn } = require('./middlewares')
 const router = express.Router()
 
 //게임 등록 localhost:8000/game
-router.post('/', isLoggedIn, async (req, res) => {})
+router.post('/', isLoggedIn, async (req, res) => {
+  const {
+    selectedGymId,
+    selectedDate,
+    selectedTime,
+    maximumPeople,
+    minimumPeople,
+  } = req.body
+  //   console.log(req.body)
+  try {
+    console.log('asdf')
+  } catch (error) {}
+})
+
+module.exports = router
