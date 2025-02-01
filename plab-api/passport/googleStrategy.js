4218965381b5e7fe229e9fd97c1fe97a54961d52
psb6396 <psb6396@naver.com>
2025-02-02 04:31:33 +0900

    asdf

--- plab-api/passport/googleStrategy.js
+++ plab-api/passport/googleStrategy.js
@@ -20,17 +20,22 @@ module.exports = () => {
             // 구글 플랫폼에서 로그인 했고 & snsId필드에 구글 아이디가 일치할경우
             where: { snsId: profile.id, accountType: 'GOOGLE' },
           })
-          // user객체에서 find해서
+          // SocialAccount객체에서 find해서
           // 이미 가입된 구글 프로필이면 성공
           if (exSocialAccount) {
-            done(null, exSocialAccount) // 로그인 인증 완료
+            const exUser = await User.findOne({
+              where: { id: exSocialAccount.userId },
+            })
+            done(null, exUser) // 로그인 인증 완료
           } else {
-            // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
+            // 가입되지 않는 유저면 SocialAccount와 User를 각각 create 하고 로그인을 시킨다
+            await SocialAccount.create({
+              snsId: profile.id,
+              accountEmail: profile?.emails[0].value,
+              accountType: 'GOOGLE',
+            })
             const newUser = await User.create({
-              email: profile?.email[0].value,
-              nick: profile.displayName,
-              // snsId: profile.id,
-              provider: 'google',
+              //email 부분을 어떻게 해야하려나.......
             })
             done(null, newUser) // 회원가입하고 로그인 인증 완료
           }
