10730baa065a7ff8d6ebfd10057de5580e8b94d6
psb6396 <psb6396@naver.com>
2025-02-02 02:27:29 +0900

    asdf

--- plab-api/routes/auth.js
+++ plab-api/routes/auth.js
@@ -108,20 +108,21 @@ router.get(
   passport.authenticate('google', { scope: ['profile', 'email'] })
 )
 
-router.get(
-  '/google/callback',
-  isNotLoggedIn,
-  passport.authenticate('google', { failureRedirect: '/' }),
-  (req, res) => {
-    // Successful authentication, redirect home.
-    res.redirect('/') // 여기도 나중에 손봐줘야할 듯
-    // res.redirect('http://localhost:3000')
-    // res.json({
-    //   success: true,
-    //   message: '구글 로그인 성공',
-    // })
-  }
-)
+router.get('/google/callback', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate(
+    'google',
+    { failureRedirect: '/' },
+    (authError, user, info) => {
+      // Successful authentication, redirect home.
+      res.redirect('/') // 여기도 나중에 손봐줘야할 듯
+      // res.redirect('http://localhost:3000')
+      // res.json({
+      //   success: true,
+      //   message: '구글 로그인 성공',
+      // })
+    }
+  )(req, res, next)
+})
 
 //로그아웃 localhost:8000/auth/logout
 router.get('/logout', isLoggedIn, async (req, res, next) => {
